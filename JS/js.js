569f89a3329130f4332f3f40dffd7704db8a57ea
Emanuel Alves <ealves502@gmail.com>
2024-09-26 23:26:06 -0300

    alteração dos buttons e etc.

--- JS/js.js
+++ JS/js.js
@@ -1,20 +1,37 @@
 let productRequestMessage = ""; // Armazena a mensagem fixa que será enviada para o WhatsApp
+let auxMessage = "";
 
 // Função para abrir o pop-up principal ou outros pop-ups
-function openPopup(popupId = "popup", message = "") {
+function openPopup(popupId = "popup", aplicativo = "", plano = "") {
   // Fecha qualquer pop-up aberto
   closePopup();
 
+  auxMessage = getMessage(aplicativo,plano);
   // Se uma mensagem for passada, armazene-a e exiba no pop-up
-  if (message) {
-    productRequestMessage = message;
-    document.getElementById("product-request-message").innerText =
-      productRequestMessage;
+  if (auxMessage) {
+    productRequestMessage = auxMessage;
   }
 
   // Abre o pop-up com o ID fornecido
   document.getElementById(popupId).style.display = "block";
   document.getElementById("popup-overlay").style.display = "block";
+}
+
+function getMessage(aplicativo = "", plano = "") {
+  const currentHour = new Date().getHours(); // Pega a hora atual
+  let greeting;
+
+  if (currentHour >= 6 && currentHour < 12) {
+    greeting = "Bom dia";
+  } else if (currentHour >= 12 && currentHour < 18) {
+    greeting = "Boa tarde";
+  } else {
+    greeting = "Boa noite";
+  }
+
+  // Concatena a saudação com a mensagem personalizada
+  const message = `${greeting}, gostaria de adquirir o plano ${plano} do aplicativo ${aplicativo}.`;
+  return message;
 }
 
 // Função para fechar todos os pop-ups
